c561d1eb6b723e6ce1447693940327d907a400ce
Luiz Paulo Prado <lp1717@gmail.com>
2025-10-15 20:38:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,4 @@ def hello():
     print("Hello LP!")
 
 
-def hello_if(number: int):
-    if number == 0:
-        print("Zero")
-    elif number == 1:
-        print("One")
-    else:
-        print("Another number: ", end=str(number))
-
-
-print("Hello World!")
 hello()
-hello_if(51)
